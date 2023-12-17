0ef8195452b871e70c25de830da85d4fdca1533c
Yemi <yemi@lagotechsolutions.com>
2023-12-17 11:39:07 -0600

    Update2 Maven_Script.sh

--- Maven_Script.sh
+++ Maven_Script.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-sudo hostnamectl set-hostname maven
-sudo su - ec2-user
 cd /opt
 sudo yum install wget tree unzip git-all -y
+sleep 3
 sudo yum install java -y
+sleep 3
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.zip
+sleep 3
 sudo unzip apache-maven-3.9.5-bin.zip
+sleep 3
 sudo rm -rf apache-maven-3.9.5-bin.zip
 sudo mv apache-maven-3.9.5/ maven
 cd /home/ec2-user
 echo "export PATH=$PATH:/opt/maven/bin" >> ~/.bash_profile
 source ~/.bash_profile
 git clone https://github.com/Lagotechsolutions/Java_Projects.git
-cd Java_Projects
+sleep 3
+cd /home/ec2-user/Java_Projects
 mvn clean package
+sleep 5
+sudo hostnamectl set-hostname maven
