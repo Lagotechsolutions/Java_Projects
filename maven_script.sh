d3edaca670790bd5a5547ab4a2b2e825c08c00f3
Yemi <yemi@lagotechsolutions.com>
2023-12-18 14:46:39 -0600

    Update maven_script.sh

--- maven_script.sh
+++ maven_script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+cd /opt
+sudo yum install wget tree unzip git-all -y
+sleep 3
+sudo yum install java -y
+sleep 3
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.zip
+sleep 3
+sudo unzip apache-maven-3.9.5-bin.zip
+sleep 3
+sudo rm -rf apache-maven-3.9.5-bin.zip
+sudo mv apache-maven-3.9.5/ maven
+cd /home/ec2-user
 echo "export PATH=$PATH:/opt/maven/bin" >> ~/.bash_profile
 source ~/.bash_profile
-git clone https://github.com/Lagotechsolutions/Java_Projects.git
-sleep 5
 cd /home/ec2-user/Java_Projects/lagotech-web-app/
-sleep 3
 mvn clean package
+sleep 5
+sudo sed -i '132r /home/ec2-user/Java_Projects/tomcat_server.xml' /opt/maven/conf/settings.xml
 sleep 5
 sudo hostnamectl set-hostname maven
