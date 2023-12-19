5f07196d514bef333ae8aa46355cfeb74ec69232
Yemi <yemi@lagotechsolutions.com>
2023-12-19 00:31:29 -0600

    Update maven_script.sh

--- maven_script.sh
+++ maven_script.sh
@@ -11,11 +11,14 @@ sleep 3
 sudo rm -rf apache-maven-3.9.5-bin.zip
 sudo mv apache-maven-3.9.5/ maven
 cd /home/ec2-user
-echo "export PATH=$PATH:/opt/maven/bin" >> /etc/profile
-source /etc/profile
+sudo chown ec2-user:ec2-user /etc/profile
+sudo chmod 775 /etc/profile
+sudo echo "export PATH=$PATH:/opt/maven/bin" >> /etc/profile
+sudo source /etc/profile
 cd /home/ec2-user/Java_Projects/lagotech-web-app/
 mvn clean package
 sleep 5
 sudo sed -i '132r /home/ec2-user/Java_Projects/tomcat_server.xml' /opt/maven/conf/settings.xml
 sleep 5
+#sudo scp -i <private_key_tomcat1> /home/ec2-user/Java_Projects/lagtlagotech-web-app.war ec2-user@<tomcat_ip>:/opt/tomcat9/webapps
 sudo hostnamectl set-hostname maven
