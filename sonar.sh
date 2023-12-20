e48c924e8ec8928f55f8599eec2a55c25c105948
Yemi <yemi@lagotechsolutions.com>
2023-12-19 23:22:41 -0600

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -4,10 +4,11 @@ sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication
 sudo service sshd restart
 sleep 3
 cd /opt
-#Install Java for SonaQube to run
+#Install CLI utilities and runtime for SonaQube to run
 sudo yum install unzip wget git-all -y
 sleep 3
-sudo yum install  java -y
+sudo wget https://download.oracle.com/java/17/archive/jdk-17.0.9_linux-aarch64_bin.tar.gz
+sudo tar -zxvf jdk-17.0.9_linux-aarch64_bin.tar.gz
 sleep 5
 #Download and extract SonarQube
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.8.zip
@@ -26,10 +27,9 @@ sudo useradd sonar
 sudo echo "sonar ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/sonar
 #Grant file permissions to sonarqube folder for sonar user
 sudo chown -R sonar:sonar /opt/sonarqube/
-# Create a password for sonar user
-sudo passwd sonar
 #Start Sonarqube
 su -c '/opt/sonarqube/bin/linux-x86-64/sonar.sh start' sonar
-su -c '/opt/sonarqube/bin/linux-x86-64/sonar.sh status' sonar
 #switch to sonar user
 sudo su - sonar
+# Create a password for sonar user
+sudo passwd sonar
