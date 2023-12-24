a7cf1fda840d6945ab97aaff835651763fc90bbf
Yemi <yemi@lagotechsolutions.com>
2023-12-23 20:28:05 -0600

    Update tomcat_users.sh

--- tomcat_users.sh
+++ tomcat_users.sh
@@ -1,4 +1,4 @@
 <role rolename="manager-gui"/>
 <role rolename="manager-script"/>
 <role rolename="admin"/>
-<user username="landmark" password="admin123" roles="manager-gui,manager-script,admin"/>
+<user username="lagotech" password="admin123" roles="manager-gui,manager-script,admin,admin"/>
